18f485f9d36952f70bf3333d07cfffff919568fa
Thirsadha Saraal <24124061@srcas.ac.in>
2026-03-09 09:57:38 +0530

    Add files via upload

---
+++ HelloJenkins.java
@@ -0,0 +1,15 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int sum;
+
+        sum = a + b;
+
+        System.out.println("Hello from Jenkins Build!");
+        System.out.println("Number A: " + a);
+        System.out.println("Number B: " + b);
+        System.out.println("Sum is: " + sum);
+    }
+}
